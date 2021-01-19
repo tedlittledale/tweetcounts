7d81ab751e813a9010df3061b177eb70895444c5
Ted Littledale <ted@unrest.tech>
2021-01-19 10:57:18 +0000

    favicons

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,27 @@ export default function Home() {
           property="og:image"
           content="https://res.cloudinary.com/dc1yk89ve/image/upload/v1611052322/Screenshot_2021-01-19_at_10.22.23_dwsbss.png"
         />
+        <link rel="icon" href="animated-favicon.gif" type="image/gif"></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Timeline title="Index Page" linkTo="/other" />
     </>
